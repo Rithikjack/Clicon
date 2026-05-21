5674b64629ffd850898ff278a3872cb4c2f0337b
Vipin Jones T <vipinjones133@gmail.com>
2026-05-21 15:51:57 +0530

    footer design completed

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from '../Components/Home/Navbar'
+import Footer from '../Components/Home/Footer'
+import Box from '@mui/material/Box';
+
 
 const HomePage = () => {
   return (
     <>
-    <Navbar/>
+      <Navbar />
+      <Box>
+        <Footer />
+
+      </Box>
     </>
   )
 }
